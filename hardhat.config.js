17b1ac44f4414462ee7c1cda9bfbe8fb84ab9b04
yumirkov <yumirkov@gmail.com>
2022-11-16 08:27:43 +0200

    added NFT implementation contract, deploy script, hardhat config, nft metadata json etc.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config("./.env");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.2",
+    solidity: "0.8.2",
+    networks: {
+        mumbai: {
+            url: process.env.MUMBAI_RPC,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,18 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
-async function main() {}
+async function main() {
+    const contractFactory = await ethers.getContractFactory("SuperMarioWorld");
+    const contract = await contractFactory.deploy("SuperMarioWorld", "SPRM");
+    await contract.deployed();
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+    console.log(`Success! Contract was deployed to: ${contract.address}`);
+
+    await contract.mint("https://ipfs.io/ipfs/QmeX9DWdk4nt46CHR5AsezRywjg6AX8km8aic5aqNikA5e");
+
+    console.log("NFT successfully minted");
+}
+
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
